01d65569adb091eed998322c6ade84eac83a9382
HMN <stookie@naver.com>
2026-05-14 16:57:34 +0900

    2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080',
-  },
-  output: 'standalone'
+  }
+
 }
 
 export default nextConfig
