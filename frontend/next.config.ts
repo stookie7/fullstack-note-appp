877b6570a6b338865a9e78bcd317130f4f1d99dc
HMN <stookie@naver.com>
2026-05-14 13:20:37 +0900

    2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // 환경변수: 로컬은 .env.local, 운영은 .env.production
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080',
   },
+  output: 'standalone'
 }
 
 export default nextConfig
